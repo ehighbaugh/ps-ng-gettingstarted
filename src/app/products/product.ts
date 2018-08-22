14a186310c2cdc5522ba20b8aad109ee93d3deaa
Erin Highbaugh <33356214+ehighbaugh@users.noreply.github.com>
2018-08-22 12:24:37 -0400

    created IProduct interface

---
+++ src/app/products/product.ts
@@ -0,0 +1,10 @@
+export interface IProduct {
+  productID: number;
+  productName: string;
+  productCode: string;
+  releaseDate: Date;
+  price: number;
+  description: string;
+  starRating: number;
+  imageUrl: string;
+}
