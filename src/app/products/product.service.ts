85b2c74751806bd1f0036a0f756bc42731f825b3
Erin Highbaugh <33356214+ehighbaugh@users.noreply.github.com>
2018-09-02 17:16:47 -0400

    added http request and error handling to getProducts

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { catchError, tap } from 'rxjx/operators';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 import { IProduct } from './product';
 
@@ -15,6 +15,23 @@ export class ProductService {
   contstructor(private http: HttpClient) {}
 
   getProducts(): Observable<IProduct[]> {
-    return this.http.get<IProduct[]>(this.productUrl);
+    return this.http.get<IProduct[]>(this.productUrl).pipe(
+      tap(data => console.log('All: ' + JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    // in real world app, this would be sent to logging infrastructure
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      // client-side or network error
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      // backend returned unsuccessful response code
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
   }
 }
