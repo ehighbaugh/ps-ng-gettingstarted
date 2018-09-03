9d4292e578860045d52c6b97dead5e29cf15472e
Erin Highbaugh <33356214+ehighbaugh@users.noreply.github.com>
2018-09-03 15:57:27 -0400

    added router to constructor; created OnBack method

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { IProduct } from './product';
 
@@ -10,9 +11,25 @@ export class ProductDetailComponent implements OnInit {
   pageTitle = 'Product Detail';
   product: IProduct;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      'productId': id,
+      'productName': 'Red Bull',
+      'productCode': 'HEL-012',
+      'releaseDate': 'December 27, 2014',
+      'description': 'A can of red bull',
+      'price': 4.99,
+      'starRating': 2.5,
+      'imageUrl': 'https://openclipart.org/detail/283385/redbull'
+    };
   }
 
+  onBack(): void {
+    this.router.navigate(['/products']);
+  }
 }
