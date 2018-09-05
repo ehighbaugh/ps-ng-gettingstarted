a5b022afd797e6f466bafb5ea18084025b7e4d58
Erin Highbaugh <33356214+ehighbaugh@users.noreply.github.com>
2018-09-05 01:27:01 -0400

    added declarations and imports to product.module

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ProductListComponent } from './product-list.component';
+import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
+import { StarComponent } from '../shared/star.component';
+import { ProductDetailComponent } from './product-detail.component';
+
+
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent,
+    ConvertToSpacesPipe,
+    StarComponent
+  ]
 })
 export class ProductModule { }
