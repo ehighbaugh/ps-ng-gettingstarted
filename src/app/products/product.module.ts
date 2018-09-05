fd998650b7f0e8d1f45c83af7d16d00d8bf1bd66
Erin Highbaugh <33356214+ehighbaugh@users.noreply.github.com>
2018-09-05 01:33:03 -0400

    moved code to product.module from app.module

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -5,11 +5,21 @@ import { ProductListComponent } from './product-list.component';
 import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
 import { StarComponent } from '../shared/star.component';
 import { ProductDetailComponent } from './product-detail.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ProductDetailGuard } from './product-detail.guard';
 
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id',
+        canActivate: [ ProductDetailGuard ],
+        component: ProductDetailComponent },
+    ]),
   ],
   declarations: [
     ProductListComponent,
